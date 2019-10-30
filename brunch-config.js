1d4f0854d0493241820ac9eda267f2c4bff8d936
tolyo <anatolyostrovsky@gmail.com>
2019-10-30 17:12:52 +0200

    Init

---
+++ brunch-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  files: {
+    javascripts: {
+      joinTo: {
+        'js/app.js':  [/^(?!app)/,/^app/]
+      }
+    },
+    stylesheets: {joinTo: 'app.css'}
+  },
+
+  plugins: {
+    babel: {
+      ignore: [/vendor/]
+    }
+  }
+};
